50a6c1a8e0c98cc9fb3bc6c45ba894c71ddaad3b
DarkMode : On <muffintoppings36@gmail.com>
2022-01-01 22:36:49 +0800

    Install devtools and lombok

--- src/main/java/com/zoren/postgresql/model/BaseModel.java
+++ src/main/java/com/zoren/postgresql/model/BaseModel.java
@@ -1,14 +1,22 @@
 package com.zoren.postgresql.model;
 
+import java.io.Serializable;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
 @MappedSuperclass
-public class BaseModel {
+public class BaseModel implements Serializable {
     public static final long serialVersionUID = 46L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 }
--- src/main/java/com/zoren/postgresql/model/Employee.java
+++ src/main/java/com/zoren/postgresql/model/Employee.java
@@ -5,8 +5,18 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
 @Table
 @Entity
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class Employee extends BaseModel {
     private String name;
     private String gender;
